a97d9abccf4c85ba599d0aea62584e68e27dd026
Milkin1 <mammadov.ilkin@hotmail.com>
2021-08-19 22:16:55 +0400

    Блок 'наши фото' и 'о комнании'

--- #src/js/main.js
+++ #src/js/main.js
@@ -13,31 +13,36 @@ $(document).ready(function () {
 
 });
 
-const swiper = new Swiper('.our-services__swiper-container', {
+
+
+const swiper = new Swiper('.swiper-container', {
+    slidesPerView: 1,
     loop: true,
+    centeredSlides: true,
+    spaceBetween: 0,
     pagination: {
-        el: '.our-services__swiper-pagination',
+        el: '.swiper-pagination',
         clickable: true
     },
     navigation: {
-        nextEl: '.our-services__swiper-button-next',
-        prevEl: '.our-services__swiper-button-prev',
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
     },
     breakpoints: {
-        // when window width is >= 320px
-        320: {
-            slidesPerView: 1,
-            spaceBetween: 20
-        },
-        // when window width is >= 480px
-        480: {
+        582.99: {
             slidesPerView: 2,
-            spaceBetween: -60,
-        },
-        // when window width is >= 640px
-        993: {
-            slidesPerView: 3,
             spaceBetween: 0,
+            centeredSlides: false,
+        },
+        991.99: {
+            slidesPerView: 2,
+            spaceBetween: 0,
+            centeredSlides: false,
+        },
+        1196.99: {
+            slidesPerView: 3,
+            spaceBetween: 40,
+            centeredSlides: true,
         }
-    }
+    },
 });
